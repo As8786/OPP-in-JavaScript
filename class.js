c711de65bd542a58dbdd288d86c219eaf4fb48fc
AS <anis.kh86@gmail.com>
2019-01-06 21:47:16 +0100

    add-content

--- class.js
+++ class.js
@@ -1,1 +1,80 @@
 // // Code using Classes here
+
+//Creating a new Class User
+// The first letter of the class name must to be an uppercase
+
+class User {
+  // declare the constructor method for class User to set up proprieties
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.counter = 0;
+    this.isLogged = false;
+  }
+  // LogIn Method
+  login() {
+    console.log(this.name, " logged");
+    // Method LogIn return this(courrent instance) to can make a chain of method(call anoter method after the end of execution of the previous one)
+    return this;
+  }
+  // Login Method
+  logOut() {
+    console.log(this.name, " logged out");
+    return this;
+  }
+  // Like Method
+  like() {
+    this.counter++;
+    console.log(this.name, " liked ", this.counter, " times");
+    return this;
+  }
+
+  // Define Method accessible by the Class not by the instance
+  static getStatus() {
+    console.log("this is User Class");
+  }
+}
+
+// Create user by using new User()
+// new operator has three roles :
+// first one  : create a empty object {}
+// second one  : set the this to the created empty object
+// third one : execute the constructor method
+
+let userOne = new User("user1", "user1@user1.com", "user1test");
+let userTwo = new User("user2", "user2@user2.com", "user2test");
+let userThree = new User("user3", "user3@user3.com", "user3test");
+
+// Creating a chain methods
+// By returning this on every method, we can create a method chain
+userOne
+  .login()
+  .like()
+  .like()
+  .like()
+  .logOut();
+
+// Create a new class Admin that inherits from the User Class
+class Admin extends User {
+  // Set up the constrcutor method for the Admin Method
+  constructor(name, email, password, role) {
+    super(name, email, password);
+    this.role = role;
+  }
+  // Add a new method for the class Admin not accesible to class User
+  deleteUser(user) {
+    return (users = users.filter(el => user.name !== el.name));
+  }
+}
+
+let userAdmin = new Admin(
+  "admin",
+  "admin Email",
+  "admin Password",
+  "Superrr Admin"
+);
+
+let users = [userOne, userTwo, userThree, userAdmin];
+
+console.log(userAdmin);
--- prototype.js
+++ prototype.js
@@ -1,1 +1,68 @@
 // Code using prototypes here
+
+// In JS there is two type of creating objects  : Factory Function and Constructor Function
+
+// The factory function is simply a function that return an object
+/*function User(name, email, password) {
+    return {
+        name,
+        password,
+        email,
+        logIn : function () {
+            console.log("the user is logged")
+        }
+    }
+*/
+
+// The constructor function for the User Object
+// The first letter of the constructor function name must be an uppercase
+function User(name, email, password) {
+  this.name = name;
+  this.email = email;
+  this.password = password;
+
+  //To avoid repition, we declare and set up Object Methods outside the constructor function
+  /*this.logIn = function() {
+    console.log(this.name, " logged in");
+  };*/
+}
+
+// To define methods with constructor function, we set up the mothods to the prototype of the constructor to avoid repetition
+User.prototype.logIn = function() {
+  console.log(this.name, " logged in");
+};
+
+User.prototype.logOut = function() {
+  console.log(this.name, " logged out");
+};
+
+//Create a new Books instance
+let user1 = new User("user1", "user1Email", "user1Paswword");
+let user2 = new User("user2", "user2Email", "user2Paswword");
+
+//The constructor function for the Admin Object that inherits from the User Object
+function Admin(name, email, password, role) {
+  this.role = role;
+
+  // Set up the inheritance of Admin to User Class
+  User.call(this, name, email, password);
+}
+
+// Set Up the User Methods to the Admin Object
+Admin.prototype = Object.create(User.prototype);
+
+//Add an admin user
+let adminUser = new Admin(
+  "admin",
+  "adminEmail",
+  "adminPassword",
+  "SuperrAdmin"
+);
+
+// Define users Array
+let users = [user1, user2, adminUser];
+
+// Define a new Admin Method
+Admin.prototype.deleteUser = function(user) {
+  return (users = users.filter(el => el.name !== user.name));
+};
